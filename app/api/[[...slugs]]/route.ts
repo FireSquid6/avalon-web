a1202eb72bf9286ebc3feaf88060804e8b65500c
firesquid6 <jdeiss06@gmail.com>
2025-05-23 10:59:50 -0500

    add the basics of an api

---
+++ app/api/[[...slugs]]/route.ts
@@ -0,0 +1,16 @@
+import { Elysia, t } from 'elysia'
+
+const app = new Elysia({ prefix: '/api' })
+  .get('/', () => 'hello Next')
+  .post('/', ({ body }) => body, {
+    body: t.Object({
+      name: t.String()
+    })
+  })
+  .get("/data", () => {
+    return "here's some more data!";
+  })
+
+
+export const GET = app.handle
+export const POST = app.handle 
