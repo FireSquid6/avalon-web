6bcd67c1f46f26916376910cf3b05d9239418fda
firesquid6 <jdeiss06@gmail.com>
2025-06-23 00:23:52 -0500

    remove useless logs when creating socket

--- src/frontend/lib/treaty.ts
+++ src/frontend/lib/treaty.ts
@@ -6,11 +6,7 @@ export const treaty = getTreaty(url);
 
 export function getSocket(): WebSocket {
   const socketProtocol = window.location.protocol === "https:" ? "wss" : "ws";
-  console.log(socketProtocol);
-  console.log(window.location.host);
-
   const url = `${socketProtocol}://${window.location.host}/socket`;
-  console.log(url);
 
   return new WebSocket(url);
 }
